579ae948cd7ebe85429063bd883134b18574b699
Guilherme <68760390+guilhermehdev@users.noreply.github.com>
2026-08-12 22:04:18 -0300

    Inicializar SQLite e migrar portfolio.json no startup

--- ApplicationEvents.vb
+++ ApplicationEvents.vb
@@ -1,29 +1,34 @@
 ﻿Imports Microsoft.VisualBasic.ApplicationServices
+Imports System.IO
 
 Namespace My
     ' The following events are available for MyApplication:
     ' Startup: Raised when the application starts, before the startup form is created.
     ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
     ' UnhandledException: Raised if the application encounters an unhandled exception.
-    ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
+    ' StartupNextInstance: Raised when launching a single-instance application and the application is already active.
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
 
-    ' **NEW** ApplyApplicationDefaults: Raised when the application queries default values to be set for the application.
-
-    ' Example:
-    ' Private Sub MyApplication_ApplyApplicationDefaults(sender As Object, e As ApplyApplicationDefaultsEventArgs) Handles Me.ApplyApplicationDefaults
-    '
-    '   ' Setting the application-wide default Font:
-    '   e.Font = New Font(FontFamily.GenericSansSerif, 12, FontStyle.Regular)
-    '
-    '   ' Setting the HighDpiMode for the Application:
-    '   e.HighDpiMode = HighDpiMode.PerMonitorV2
-    '
-    '   ' If a splash dialog is used, this sets the minimum display time:
-    '   e.MinimumSplashScreenDisplayTime = 4000
-    ' End Sub
-
     Partial Friend Class MyApplication
+
+        Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+            Try
+                PortfolioDb.Initialize()
+
+                Dim jsonPath As String = Path.Combine(
+                    Application.StartupPath,
+                    "JSON",
+                    "portfolio.json")
+
+                If PortfolioDb.GetDatabaseRowCount() = 0 AndAlso File.Exists(jsonPath) Then
+                    Dim imported As Integer = PortfolioDb.MigrateFromJson(jsonPath)
+                    Debug.WriteLine($"SQLite: {imported} registros migrados do portfolio.json.")
+                End If
+
+            Catch ex As Exception
+                Debug.WriteLine("Erro ao inicializar SQLite: " & ex.Message)
+            End Try
+        End Sub
 
     End Class
 End Namespace
